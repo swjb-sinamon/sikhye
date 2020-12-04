d34359b4a1467a4ec0115d42f0f78928c48d849c
combbm <combbm@naver.com>
2020-12-04 17:01:33 +0900

    feat: project init

---
+++ src/atomics/Form/ScaleInput.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const ScaleInput = styled.input`
+  transform: scale(1.2);
+`;
+
+export default ScaleInput;
---
+++ src/components/MainSideBar/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import SCREEN_SIZE from '../../styles/screen-size';
+
+export const MainSidebar = styled.ul`
+  min-height: 100vh;
+  background-color: white;
+  box-shadow: 0 0 30px rgba(169, 169, 169, 0.2);
+
+  display: flex;
+  flex-direction: column;
+
+  list-style: none;
+
+  @media screen and (max-width: ${SCREEN_SIZE.SCREEN_TABLET}) {
+    min-height: 4rem;
+  }
+`;
+
+export const SideBarItemList = styled.ul<{ isOpen: boolean }>`
+  display: block;
+  @media screen and (max-width: ${SCREEN_SIZE.SCREEN_TABLET}) {
+    display: ${(props) => (props.isOpen ? 'block' : 'none')};
+  }
+`;
