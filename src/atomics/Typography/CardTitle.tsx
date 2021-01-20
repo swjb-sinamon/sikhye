e0e958f3af0659f5b91698e7f6351a61fbc4eb3c
combbm <combbm@naver.com>
2021-01-20 22:59:03 +0900

    fix: fix font-size of CardTitle when screen is mobile

--- src/atomics/Typography/CardTitle.tsx
+++ src/atomics/Typography/CardTitle.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { SCREEN_SIZE } from '../../styles';
 
 const CardTitle = styled.h2`
   font-size: 26px;
@@ -6,6 +7,10 @@ const CardTitle = styled.h2`
   font-family: 'NanumSquareRound', sans-serif;
 
   margin-bottom: 12px;
+
+  @media screen and (max-width: ${SCREEN_SIZE.SCREEN_MOBILE}) {
+    font-size: 24px;
+  }
 `;
 
 export default CardTitle;
